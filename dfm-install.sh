3100c26eee05e63c354870921d69ef340ce64f8b
JD <jdiamond_11@comcast.net>
2019-12-07 13:12:25 -0500

    Cleaned up install script

--- dfm-install.sh
+++ dfm-install.sh
@@ -18,22 +18,3 @@ else
     echo
     exit
 fi
-
-dir2=$(ls -AlF /home/$USER/ | grep "bin/")
-
-if [[ "$dir2" == "bin/" ]]; then
-    echo
-    sleep 1
-    echo "All Done..."
-    echo
-    exit
-else
-    pushd /home/$USER/ &> /dev/null
-    mkdir bin/
-    cp dfm /home/$USER/bin/dfm
-    popd &> /dev/null
-    echo
-    echo "All Done..."
-    echo
-    exit
-fi
